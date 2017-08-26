192fec1987c2e7cc7b0627600115bd9ec8a17394
maomao <fox91119@gmail.com>
2017-08-26 17:18:26 +0800

    2d

--- hello.py
+++ hello.py
@@ -1,69 +1,24 @@
 # hello.py
 
 from flask import Flask
-from flask import redirect
-from flask import make_response, Response
-from flask import abort
+from flask_script import Manager
+#from flask.ext.script import Manager
 
 
 app = Flask(__name__)
+manager = Manager(app)
 
-'''
-@app.route("/")
-def index():
-    return "<h1>Bad Request</h1>", 400
-'''
-'''
-@app.route("/")
-def index():
-    return "<h1>Redirect</h1>", 302, {"Location": "http://www.google.com"}
-'''
 
 @app.route("/")
 def index():
-    return redirect("http://www.google.com")
+    return "<h1>Hello Word</h1>"
 
 
-@app.route("/no_cookie")
-def no_cookie():
-    response = make_response("<h1>This document doesn't carry a cookie!</h1>")
-    return response
-
-'''
-@app.route("/has_cookie")
-def has_cookie():
-    response = make_response("<h1>This document carries a cookie!</h1>")
-    response.set_cookie("answer", "42")
-    return response
-'''
-
-@app.route("/has_cookie")
-def has_cookie():
-    data = "<h1>This document carries a cookie!</h1>"
-    headers = {}
-    headers["Set-Cookie"] = "answer=45"
-    return Response(data, headers=headers)
-
-
-def load_user(uid):
-    try:
-        uid = int(uid)
-        if uid == 1:
-            return "Maomao"
-        elif uid == 2:
-            return "Alicia"
-    except BaseException:
-        return
-
-
-@app.route("/user/<uid>")
-def get_user(uid):
-    user = load_user(uid)
-    if not user:
-        abort(400)
-    else:
-        return "<h1>Hello, {}!</h1>".format(user)
+@manager.command
+def hello():
+    """Just say hello"""
+    print("hello")
 
 
 if __name__ == "__main__":
-    app.run(threaded=True, debug=True)
+    manager.run()
