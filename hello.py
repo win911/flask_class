b0bcf5b93c67ba8569ef6eb725780daeb8a4bc83
maomao <fox91119@gmail.com>
2017-08-27 17:57:52 +0800

    3d

--- hello.py
+++ hello.py
@@ -1,38 +1,22 @@
 # hello.py
 
-from flask import Flask, render_template, abort
+from datetime import datetime
+
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 #from flask.ext.bootstrap import Bootstrap
-#from flask import url_for
+#from flask.ext.moment import Moment
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template("500.html"), 500
-
-
-@app.route("/test")
-def test():
-    abort(500)
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
-
-
-@app.route("/user/<name>")
-def user(name):
-    return render_template("user.html", name=name)
+    return render_template('index.html', current_time = datetime.utcnow())
 
 
 if __name__ == "__main__":
