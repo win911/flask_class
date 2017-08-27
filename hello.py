ebf38471c274193c83eeda53502e731c77371ef7
maomao <fox91119@gmail.com>
2017-08-27 15:13:50 +0800

    3b

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+registered_users = ["maomao", "alicia"]
 
 
 @app.route("/")
@@ -13,48 +14,14 @@ def index():
 
 @app.route("/user/<name>")
 def user(name):
+    if name not in registered_users:
+        name = None
     return render_template("user.html", name=name)
 
-'''
-@app.route("/test")
-def test():
-    mydict = {"key": "This is a secret"}
-    mylist = [1, 2, 3, 4]
-    myintvar = 0
 
-    class Myobj():
-        def method1(self):
-            return "I'm a instance method."
-
-        @staticmethod
-        def method2():
-            return "I'm a static method."
-
-        @classmethod
-        def method3(cls, value):
-            return "I'm a class method, get value {}".format(value)
-
-    context = {
-        "mydict": mydict,
-        "mylist": mylist,
-        "myintvar": myintvar,
-        "instance": Myobj(),
-        "class": Myobj
-    }
-
-    return render_template("test.html", **context)
-'''
-
-@app.route("/test")
-def test():
-    code = "<h1>Hello</h1>"
-    return render_template("test.html", code=code)
-
-
-@app.route("/vulnerable")
-def vulnerable():
-    code = """<script>alert("I'm a hacker.")</script>"""
-    return render_template("test.html", code=code)
+@app.route("/users")
+def users():
+    return render_template("users.html", users=registered_users)
 
 
 if __name__ == "__main__":
