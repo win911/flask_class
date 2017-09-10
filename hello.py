42a607c9d31b2b33f1f7ef8e0c0b24915b903f5f
maomao <fox91119@gmail.com>
2017-09-10 08:47:37 +0800

    4a

--- hello.py
+++ hello.py
@@ -1,22 +1,31 @@
 # hello.py
 
-from datetime import datetime
-
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-#from flask.ext.bootstrap import Bootstrap
-#from flask.ext.moment import Moment
+from flask_wtf import FlaskForm
+#from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hard to guess string"    # Important for CSRF token
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("index.html", form=form, name=name)
 
 
 if __name__ == "__main__":
