70179d3694dc58a67c3bc65a10ae8b42f7d0c73b
maomao <fox91119@gmail.com>
2017-09-10 08:54:02 +0800

    4b

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 # hello.py
 
 from flask import Flask, render_template
+from flask import session, redirect, url_for
+from flask import flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 #from flask.ext.wtf import Form
@@ -17,7 +19,7 @@ class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[Required()])
     submit = SubmitField("Submit")
 
-
+'''
 @app.route("/", methods=["GET", "POST"])
 def index():
     name = None
@@ -26,6 +28,18 @@ def index():
         name = form.name.data
         form.name.data = ""
     return render_template("index.html", form=form, name=name)
+'''
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, name=session.get("name"))
 
 
 if __name__ == "__main__":
