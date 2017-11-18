8eceaf77d70764f9ab2503544bd45880f40741a1
maomao <fox91119@gmail.com>
2017-11-18 10:01:17 +0800

    6c

--- hello_db.py
+++ hello_db.py
@@ -1,33 +1,44 @@
 # hello_db.py
 
 import os
-
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_script import Shell
 from flask_sqlalchemy import SQLAlchemy
-#from flask.ext.sqlalchemy import SQLAlchemy
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-manager = Manager(app)
-app.config["SQLALCHEMY_DATABASE_URI"] =\
-    "sqlite:///" + os.path.join(basedir, "data.sqlite")
-app.config["SQLAlCHEMY_COMMIT_ON_TEARDOWN"] = True
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ECHO"] = True
 
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 db = SQLAlchemy(app)
 
-###### Single ForeignKey ######
-"""
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    #users = db.relationship('User') # relationship (from Role link to User)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -38,43 +49,49 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    #role = db.relationship('Role') # relationship (from User link to Role)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-"""
-###############################
-
-##### Multiple ForeignKeys #####
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-
-    #users_primary_role = db.relationship('User', primaryjoin='Role.id==User.primary_role_id')
-    #users_secondary_role = db.relationship('User', primaryjoin='Role.id==User.secondary_role_id')
-    users_primary_role = db.relationship('User', backref='primary_role', primaryjoin='Role.id==User.primary_role_id')
-    users_secondary_role = db.relationship('User', backref='secondary_role', primaryjoin='Role.id==User.secondary_role_id')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    primary_role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    secondary_role_id =  db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    #primary_role = db.relationship('Role', foreign_keys='User.primary_role_id')
-    #secondary_role = db.relationship('Role', foreign_keys='User.secondary_role_id')
 
     def __repr__(self):
         return '<User %r>' % self.username
 
-################################
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()     # query user
+        if user is None:
+            admin_role = Role.query.filter_by(name='Admin').first()      # query role
+            user = User(username=form.name.data, role_id=admin_role.id)  # add user
+            db.session.add(user)     # add to database
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
+
+
+@app.route('/db/users')
+def list_users_in_db():
+    users = User.query.all()
+    return "<br>".join([user.username for user in users])
+
 
 if __name__ == '__main__':
     manager.run()
