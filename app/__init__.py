232bf1f5fff82b119a6964bc6088aa62b4f33753
maomao <fox91119@gmail.com>
2017-12-07 16:43:32 +0800

    8b

--- app/__init__.py
+++ app/__init__.py
@@ -23,9 +23,11 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
-    
+
     # blueprint registration
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
---
+++ app/auth/views.py
@@ -0,0 +1,9 @@
+# app/auth/views.py
+
+from flask import render_template
+
+from . import auth
+
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')
