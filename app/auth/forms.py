5d60f3dcafad4e2195cdaba2f312d1a216aa9794
maomao <fox91119@gmail.com>
2017-12-07 18:10:22 +0800

    8h

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -31,3 +31,10 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[Required()])
+    password = PasswordField('New password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm new password', validators=[Required()])
+    submit = SubmitField('Update Password')
