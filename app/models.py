e2dca4c498f2a36f814f2a69759e1da2b4b190bb
maomao <fox91119@gmail.com>
2017-12-07 16:37:30 +0800

    8a

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 # app/models.py
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from . import db
 
 
@@ -18,6 +20,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password, method="pbkdf2:sha1")
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
