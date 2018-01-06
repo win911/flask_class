0f109caee59e77d1107bdd1f1be23092ac2924f6
maomao <fox91119@gmail.com>
2018-01-06 13:03:17 +0800

    11c

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_pagedown import PageDown
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
@@ -16,6 +17,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+pagedown = PageDown()
 
 
 def create_app(config_name):
@@ -28,6 +30,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
     # blueprint registration
     from .main import main as main_blueprint
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 # app/models.py
 
+import bleach
 from datetime import datetime
 import hashlib
+from markdown import markdown
 
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
@@ -64,6 +66,7 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
 
     @staticmethod
     def generate_fake(count=100):
@@ -79,6 +82,18 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 class User(UserMixin, db.Model):
