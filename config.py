b181e8e9338094a3abbba245055508759fe0c0ee
maomao <fox91119@gmail.com>
2018-01-06 12:54:35 +0800

    11b

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class Config(object):
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
